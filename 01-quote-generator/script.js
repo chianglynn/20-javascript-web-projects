96cff09532afb43673afad6c23546af6c4ef66bf
Chiang Lynn <lynnchiangtw@gmail.com>
2021-03-12 13:05:56 +0800

    Add loading animation

--- 01-quote-generator/script.js
+++ 01-quote-generator/script.js
@@ -3,14 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
 
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // Solution 1: Use API Quotes instead of Local Quotes
 let apiUrlQuotes = [];
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -21,8 +33,21 @@ async function getQuotes() {
     }
 }
 
+/*
+// Solution 2: Use Local Quotes
 // Show New Quote
 function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    console.log(quote);
+}
+
+newQuote();
+*/
+
+// Show New Quote
+function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiUrlQuotes[Math.floor(Math.random() * apiUrlQuotes.length)];
 
@@ -41,6 +66,9 @@ function newQuote() {
     }
 
     quoteText.textContent = quote.text;
+
+    // Set Quote, Hide Loader
+    complete();
 }
 
 // Tweet quote
@@ -56,15 +84,3 @@ getQuotes();
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-
-/*
-// Solution 2: Use Local Quotes
-// Show New Quote
-function newQuote() {
-    // Pick a random quote from apiQuotes array
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
-}
-
-newQuote();
-*/
